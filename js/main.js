3291e3470de1e9131b66c0c6a314205fa73b39d8
Alla Panova <alla.mixalvna@gmail.com>
2019-06-21 01:44:49 +0300

    выполнено задание Личный проект: подробности

--- js/main.js
+++ js/main.js
@@ -3,15 +3,38 @@
 var NUMBER_OF_ADS = 8;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+var MAIN_PIN_WIDTH = 65;
+var MAIN_PIN_HEIGHT = 65;
+var MAIN_PIN_AFTER_HEIGHT = 22;
+
+var adForm = document.querySelector('.ad-form');
+var mainPin = document.querySelector('.map__pin--main');
+var apartmentTypes = ['palace', 'flat', 'house', 'bungalo'];
+var map = document.querySelector('.map');
+var adFormElements = adForm.querySelectorAll('.ad-form__element');
+var inputAddress = adForm.querySelector('input[name = address]');
+
+var addDisabledAttribute = function () {
+  adForm.querySelector('.ad-form-header').setAttribute('disabled', '');
+
+  adFormElements.forEach(function (element) {
+    element.setAttribute('disabled', '');
+  });
+};
+addDisabledAttribute();
+
+var deleteDisabledttribute = function () {
+  adForm.querySelector('.ad-form-header').removeAttribute('disabled', '');
+
+  adFormElements.forEach(function (element) {
+    element.removeAttribute('disabled', '');
+  });
+};
+
 
 var randomInteger = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
-
-var apartmentTypes = ['palace', 'flat', 'house', 'bungalo'];
-
-var map = document.querySelector('.map');
-map.classList.remove('map--faded'); /* Временно активный режим */
 
 var getAdsArray = function (numberOfAds) {
   var ads = [];
@@ -59,3 +82,26 @@ var appendAd = function (ads) {
 };
 
 appendAd(getAdsArray(NUMBER_OF_ADS));
+
+
+var leftCoordinate = mainPin.style.left;
+leftCoordinate = leftCoordinate.substring(0, leftCoordinate.length - 2);
+
+var topCoordinate = mainPin.style.top;
+topCoordinate = topCoordinate.substring(0, topCoordinate.length - 2);
+
+var middleCoordinateX = Number(leftCoordinate) + Number(MAIN_PIN_WIDTH / 2); /* коордиината Х середины главной метки */
+var middleCoordinateY = Number(topCoordinate) + Number(MAIN_PIN_HEIGHT / 2); /* коордиината Y середины главной метки */
+var bottomCoordinateX = Number(topCoordinate) + MAIN_PIN_HEIGHT + MAIN_PIN_AFTER_HEIGHT; /* координата Х острого конца главной метки*/
+
+inputAddress.setAttribute('value', middleCoordinateX + ', ' + middleCoordinateY); /* присвоение значения полю Адрес при неактивном состоянии*/
+
+mainPin.addEventListener('mouseup', function () {
+  mainPin.addEventListener('click', function () {
+    map.classList.remove('map--faded');
+    adForm.classList.remove('ad-form--disabled');
+    deleteDisabledttribute();
+  });
+
+  inputAddress.setAttribute('value', middleCoordinateX + ', ' + bottomCoordinateX); /* присвоение значения полю Адрес при активном состоянии*/
+});

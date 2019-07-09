589f1911688c97ea6700d177a4b789a9c550eb73
Alla Panova <alla.mixalvna@gmail.com>
2019-07-10 01:43:34 +0300

    готова часть фильтров для меток

--- js/load.js
+++ js/load.js
@@ -1,7 +1,10 @@
 'use strict';
 (function () {
   var mapFilters = document.querySelector('.map__filters');
-  var housingType = mapFilters.querySelector('select[name = housing-type]');
+  var housingType = mapFilters.querySelector('#housing-type');
+  var housingPrice = mapFilters.querySelector('#housing-price');
+  var housingRooms = mapFilters.querySelector('#housing-rooms');
+  var housingGuests = mapFilters.querySelector('#housing-guests');
 
   var onError = function () {
     window.message.showErrorMessage();
@@ -9,13 +12,26 @@
 
   var onSuccess = function (data) {
     window.allPins = data;
-
+    console.log(window.allPins);
     window.pin.appendAdPin(data.slice(0, 5));
 
     housingType.addEventListener('change', function () {
-      window.pinManage.reloadPins(housingType.options[housingType.selectedIndex].value);
+      window.pinManage.reloadPins();
+    });
+
+    housingRooms.addEventListener('change', function () {
+      window.pinManage.reloadPins();
+    });
+
+    housingGuests.addEventListener('change', function () {
+      window.pinManage.reloadPins();
+    });
+
+    housingPrice.addEventListener('change', function () {
+      window.pinManage.reloadPins();
     });
   };
+
 
   window.exchange.load('https://js.dump.academy/keksobooking/data', onSuccess, onError);
 })();
--- js/pin_manage.js
+++ js/pin_manage.js
@@ -2,6 +2,7 @@
 
 (function () {
 
+
   var removePin = function () {
     var mapPins = document.querySelector('.map__pins');
     var pins = mapPins.querySelectorAll('button.map__pin:not(.map__pin--main)');
@@ -13,21 +14,66 @@
     });
   };
 
-  var filterPins = function (filter) {
+
+  var filterByPrice = function (data, filter) {
     return filter === 'any'
-      ? window.allPins
-      : window.allPins.filter(function (pin) {
+      ? data
+      : data.filter(function (pin) {
+        if (filter === 'low') {
+          var result = pin.offer.price < 10000;
+        } else if (filter === 'middle') {
+          result = pin.offer.price >= 10000 && pin.offer.price < 50000;
+        } else if (filter === 'high') {
+          result = pin.offer.price >= 50000;
+        }
+        return result;
+      });
+  };
+
+  var filterByRooms = function (data, filter) {
+    return filter === 'any'
+      ? data
+      : data.filter(function (pin) {
+        return String(pin.offer.rooms) === filter;
+      });
+  };
+
+  var filterByGuests = function (data, filter) {
+    return filter === 'any'
+      ? data
+      : data.filter(function (pin) {
+        return String(pin.offer.guests) === filter;
+      });
+  };
+
+  var filterByType = function (data, filter) {
+    return filter === 'any'
+      ? data
+      : data.filter(function (pin) {
         return pin.offer.type === filter;
       });
   };
 
-  var reloadPins = function (filter) {
+  var reloadPins = function () {
+    var mapFilters = document.querySelector('.map__filters');
+    var housingType = mapFilters.querySelector('#housing-type');
+    var housingPrice = mapFilters.querySelector('#housing-price');
+    var housingRooms = mapFilters.querySelector('#housing-rooms');
+    var housingGuests = mapFilters.querySelector('#housing-guests');
+
     window.pinManage.removePin();
-    window.pin.appendAdPin(filterPins(filter).slice(0, 5));
+
+    var data = window.allPins;
+    data = filterByType(data, housingType.options[housingType.selectedIndex].value);
+    data = filterByRooms(data, housingRooms.options[housingRooms.selectedIndex].value);
+    data = filterByGuests(data, housingGuests.options[housingGuests.selectedIndex].value);
+    data = filterByPrice(data, housingPrice.options[housingPrice.selectedIndex].value);
+
+    window.pin.appendAdPin(data);
   };
 
+
   window.pinManage = {
-    filterPins: filterPins,
     reloadPins: reloadPins,
     removePin: removePin
   };

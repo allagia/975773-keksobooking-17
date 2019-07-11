cad3f8fbb39839bf7076530f48f02a152fe420fa
Alla Panova <alla.mixalvna@gmail.com>
2019-07-11 18:03:37 +0300

    оживляет кнопку Очистить форму

--- js/upload.js
+++ js/upload.js
@@ -4,6 +4,7 @@
   var adForm = document.querySelector('.ad-form');
   var pageMap = document.querySelector('.map');
   var mainPin = document.querySelector('.map__pin--main');
+  var formReset = document.querySelector('.ad-form__reset');
 
   var onSuccess = function () {
 
@@ -48,5 +49,20 @@
   adForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
     window.exchange.upload(new FormData(adForm), onSuccess, onError);
+  });
+
+  formReset.addEventListener('click', function () {
+    adForm.reset();
+    mainPin.style.left = '570px';
+    mainPin.style.top = '375px';
+    window.form.setAddress();
+    document.querySelector('#price').placeholder = '1000';
+    document.querySelector('#price').min = '1000';
+    pageMap.classList.add('map--faded');
+    adForm.classList.add('ad-form--disabled');
+    window.data.addDisabledAttribute();
+    window.event.removeChild('.map', 'article');
+    window.pinManage.removePin();
+    window.pin.appendAdPin(window.allPins.slice(0, 5));
   });
 })();

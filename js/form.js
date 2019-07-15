5bca3e366e028bea5b90c2c5b77468f47892cf70
Alla Panova <alla.mixalvna@gmail.com>
2019-07-15 17:06:24 +0300

    исправляет замечания по 4 разделу ТЗ

--- js/form.js
+++ js/form.js
@@ -1,39 +1,27 @@
 'use strict';
 
 (function () {
-  var MAIN_PIN_WIDTH = 65;
-  var MAIN_PIN_HEIGHT = 65;
-  var MAIN_PIN_AFTER_HEIGHT = 22;
 
   var mainPin = document.querySelector('.map__pin--main');
   var adForm = document.querySelector('.ad-form');
   var inputAddress = adForm.querySelector('input[name = address]');
 
-  var setAddress = function (isActive) {
+  var setAddress = function () {
     var coordinateX = mainPin.style.left;
     coordinateX = coordinateX.substring(0, coordinateX.length - 2);
     var coordinateY = mainPin.style.top;
     coordinateY = coordinateY.substring(0, coordinateY.length - 2);
 
-    var x = Number(coordinateX) + Number(MAIN_PIN_WIDTH / 2);
-
-    // if (isActive) {
-    //   var y = Number(coordinateY) + MAIN_PIN_HEIGHT + MAIN_PIN_AFTER_HEIGHT;
-    // } else {
-    //   y = Number(coordinateY) + Number(MAIN_PIN_HEIGHT / 2);
-    // }
-
-    var y = (isActive
-      ? Number(coordinateY) + MAIN_PIN_HEIGHT + MAIN_PIN_AFTER_HEIGHT
-      : Number(coordinateY) + Number(MAIN_PIN_HEIGHT / 2));
+    var x = Number(coordinateX);
+    var y = Number(coordinateY);
 
     inputAddress.value = x + ', ' + y;
   };
 
   setAddress();
 
-  mainPin.addEventListener('mouseup', function () {
-    setAddress(true);
+  document.addEventListener('mouseup', function () {
+    setAddress();
   });
 
   var HousingPrice = {

48d5e8b6a3a4c551408b00ae934ac3ee7d957655
Alla Panova <alla.mixalvna@gmail.com>
2019-07-19 00:55:54 +0300

    исправления согласно замечаниям

--- js/avatar.js
+++ js/avatar.js
@@ -1,35 +1,45 @@
 'use strict';
 
 (function () {
-  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
-  var fileChooser = document.querySelector('.ad-form-header__input');
-  var preview = document.querySelector('.ad-form-header__preview img');
+  var uploadPhoto = function (element, handler) {
+    var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+    var fileChooser = document.querySelector(element);
 
-  fileChooser.addEventListener('change', function () {
-    var file = fileChooser.files[0];
-    var fileName = file.name.toLowerCase();
+    fileChooser.addEventListener('change', function () {
+      var file = fileChooser.files[0];
+      var fileName = file.name.toLowerCase();
 
-    var matches = FILE_TYPES.some(function (it) {
-      return fileName.endsWith(it);
-    });
-
-    if (matches) {
-      var reader = new FileReader();
-
-      reader.addEventListener('load', function () {
-        preview.src = reader.result;
+      var matches = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
       });
 
-      reader.readAsDataURL(file);
-    }
-  });
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          handler(reader);
+        });
+
+        reader.readAsDataURL(file);
+      }
+    });
+  };
+
+  var preview = document.querySelector('.ad-form-header__preview img');
+
+  var onAvatarUpload = function (reader) {
+    preview.src = reader.result;
+  };
+
+  uploadPhoto('.ad-form-header__input', onAvatarUpload);
 
   var removeAvatar = function () {
     preview.src = 'img/muffin-grey.svg';
   };
 
   window.avatar = {
-    remove: removeAvatar
+    remove: removeAvatar,
+    uploadPhoto: uploadPhoto
   };
 })();
--- js/photo.js
+++ js/photo.js
@@ -1,39 +1,23 @@
 'use strict';
 
 (function () {
-  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
-  var fileChooser = document.querySelector('.ad-form__input');
   var preview = document.querySelector('.ad-form__photo');
   var container = document.querySelector('.ad-form__photo-container');
 
+  var onPhotoUpload = function (reader) {
+    var image = document.createElement('img');
+    image.style.maxWidth = '100%';
+    preview.style.overflow = 'hidden';
+    preview.appendChild(image);
+    image.src = reader.result;
+    var newDiv = document.createElement('div');
+    newDiv.className = 'ad-form__photo';
+    container.appendChild(newDiv);
+    preview = newDiv;
+  };
 
-  fileChooser.addEventListener('change', function () {
-    var file = fileChooser.files[0];
-    var fileName = file.name.toLowerCase();
-
-    var matches = FILE_TYPES.some(function (it) {
-      return fileName.endsWith(it);
-    });
-
-    if (matches) {
-      var reader = new FileReader();
-
-      reader.addEventListener('load', function () {
-        var image = document.createElement('img');
-        image.style.maxWidth = '100%';
-        preview.style.overflow = 'hidden';
-        preview.appendChild(image);
-        image.src = reader.result;
-        var newDiv = document.createElement('div');
-        newDiv.className = 'ad-form__photo';
-        container.appendChild(newDiv);
-        preview = newDiv;
-      });
-
-      reader.readAsDataURL(file);
-    }
-  });
+  window.avatar.uploadPhoto('.ad-form__input', onPhotoUpload);
 
   var removePhoto = function () {
     var divs = document.querySelectorAll('.ad-form__photo');
@@ -45,7 +29,9 @@
     });
 
     preview = container.appendChild(div);
-    div.removeChild(image);
+    if (image) {
+      div.removeChild(image);
+    }
   };
 
   window.photo = {
